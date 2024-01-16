a8ebb22dddc2aaeaa50d0cf506980a0b439cb03f
Omar Ubaid <omarubaid1997@gmail.com>
2024-01-17 00:41:34 +0100

    Added train-test-split of sklearn package

--- mnist_cnn_with_dropout.py
+++ mnist_cnn_with_dropout.py
@@ -1,55 +1,65 @@
 from keras.datasets import mnist
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping
+from keras import models, layers
 import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train= x_train.reshape((60000, 28, 28, 1)).astype('float32') / 255
-x_test= x_test.reshape((10000, 28, 28, 1)).astype('float32') / 255
+x_train = x_train.reshape((60000, 28, 28, 1)).astype('float32') / 255
+x_test = x_test.reshape((10000, 28, 28, 1)).astype('float32') / 255
 
-y_train= to_categorical(y_train)
-y_test= to_categorical(y_test)
+validation_size = 10000
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=validation_size, random_state=42, shuffle= True)
 
-network= models.Sequential()
-network.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-network.add(layers.MaxPooling2D(2, 2))
-network.add(layers.Flatten())
-network.add(layers.Dense(units=128, activation='relu'))
-network.add(layers.Dropout(0.3))
-network.add(layers.Dense(units=10, activation='softmax'))
+model = models.Sequential()
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(64, (3, 3), activation='relu')),
+model.add(layers.MaxPooling2D((2))),
+model.add(layers.InputLayer(input_shape=(28, 28, 1)))
+model.add(layers.Flatten())
+model.add(layers.Dense(units=256, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(units=64, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(units=10, activation='softmax'))
 
-network.compile(optimizer='adam',
-                loss='categorical_crossentropy',
-                metrics=['accuracy'])
-
-#network.save('mnist_model5.h5')
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
-history= network.fit(x_train, y_train, epochs=5, batch_size=128, validation_data=(x_test, y_test), callbacks=[early_stopping])
+history = model.fit(x_train, y_train, epochs=10, batch_size=128, validation_data=(x_val, y_val), callbacks=[early_stopping])
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'Test Accuracy: {test_acc}')
 
 train_loss = history.history['loss']
-train_acc = history.history['accuracy']
 val_loss = history.history['val_loss']
+train_acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
-
 epochs = range(1, len(train_loss) + 1)
 
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
 plt.plot(epochs, train_loss, 'o-', color='orange', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
-plt.show()
 
+plt.subplot(1, 2, 2)
 plt.plot(epochs, train_acc, 'o-', color='orange', label='Training accuracy')
 plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
 plt.title('Training and Validation Accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
+
+plt.tight_layout()
 plt.show()
